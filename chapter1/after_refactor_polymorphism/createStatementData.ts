a76174bf79d9722e9e05e0983bfa739aaa96292b
1571min <1571min@naver.com>
2021-03-07 13:06:43 +0900

    amountFor 함수 클래스 이동 후 인라인 호출

--- chapter1/after_refactor_polymorphism/createStatementData.ts
+++ chapter1/after_refactor_polymorphism/createStatementData.ts
@@ -4,9 +4,32 @@ import { Statement } from './statement'
 class PerformanceCalculator {
   performance: PerformanceT
   play: Play
-  constructor(aPerformace: PerformanceT, aPlay: Play) {
-    this.performance = aPerformace
+  constructor(aPerformance: PerformanceT, aPlay: Play) {
+    this.performance = aPerformance
     this.play = aPlay
+  }
+
+  // 함수 옮기기 후 함수 인라인
+  get amount() {
+    let result = 0
+    switch (this.play.type) {
+      case 'tragedy':
+        result = 40000
+        if (this.performance.audience > 30) {
+          result += 1000 * (this.performance.audience - 30)
+        }
+        break
+      case 'comedy':
+        result = 30000
+        if (this.performance.audience > 20) {
+          result += 10000 + 500 * (this.performance.audience - 20)
+        }
+        result += 300 * this.performance.audience
+        break
+      default:
+        throw new Error(`알 수 없는 장르:  ${this.play.type}`)
+    }
+    return result
   }
 }
 
@@ -14,28 +37,6 @@ export default (invoice: Invoice, plays: Plays) => {
   // 계산 단계 분리를 위한 계층 쪼개기
   const playFor = (perf: PerformanceT): Play => {
     return plays[perf.playID]
-  }
-
-  const amountFor = (aPerformance: PerformanceT): number => {
-    let result = 0
-    switch (aPerformance.play.type) {
-      case 'tragedy':
-        result = 40000
-        if (aPerformance.audience > 30) {
-          result += 1000 * (aPerformance.audience - 30)
-        }
-        break
-      case 'comedy':
-        result = 30000
-        if (aPerformance.audience > 20) {
-          result += 10000 + 500 * (aPerformance.audience - 20)
-        }
-        result += 300 * aPerformance.audience
-        break
-      default:
-        throw new Error(`알 수 없는 장르:  ${aPerformance.play.type}`)
-    }
-    return result
   }
 
   const volumeCreditsFor = (perf: PerformanceT): number => {
@@ -50,7 +51,7 @@ export default (invoice: Invoice, plays: Plays) => {
     const calculator = new PerformanceCalculator(aPerformance, playFor(aPerformance))
     const result = Object.assign({}, aPerformance) // 얕은 복사로 전달
     result.play = calculator.play
-    result.amount = amountFor(result)
+    result.amount = calculator.amount
     result.volumeCredits = volumeCreditsFor(result)
     return result
   }
